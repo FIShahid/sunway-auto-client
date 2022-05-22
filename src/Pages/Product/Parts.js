73d0b52a5b56f82b784a8ac122b2eaf823d95577
Fokhrul Islam <shahidcse12@gmail.com>
2022-05-23 04:25:39 +0600

    Update stock

--- src/Pages/Product/Parts.js
+++ src/Pages/Product/Parts.js
@@ -23,6 +23,73 @@ const Parts = () => {
         }
     }, []);
 
+    const [input, setInput] = useState(0)
+    const inputValue = e => {
+        e.preventDefault()
+
+        setInput(e.target.value);
+
+    }
+
+    const increaseStock = id => {
+        console.log(input);
+        const newStock = (stock + parseInt(input));
+        if (isNaN(newStock)) {
+            setError("Input a Number")
+            return
+        }
+        else {
+            setError('')
+        }
+        console.log(parseFloat(newStock));
+        const update = { ...product, stock: newStock }
+        const url = `http://localhost:5000/parts/${partsId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(update)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setProduct({ ...product, stock: newStock })
+                
+
+            })
+
+    }
+
+    const decreaseStock = id => {
+        console.log(input);
+        const newStock = (stock - parseInt(input));
+        if (isNaN(newStock)) {
+            setError("Input a Number")
+            return
+        }
+        else {
+            setError('')
+        }
+        console.log(parseFloat(newStock));
+        const update = { ...product, stock: newStock }
+        const url = `http://localhost:5000/parts/${partsId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(update)
+        })
+            .then(res => res.json())
+            .then(data => {
+                setProduct({ ...product, stock: newStock })
+                
+
+            })
+
+    }
+
+
     const handleBooking = event => {
         event.preventDefault();
     }
@@ -42,6 +109,7 @@ const Parts = () => {
                         </div>
                     </div>
 
+
                     <div className='"card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100"'>
                         <div className='card-body'>
                             <h4 className='text-xl font-bold mx-auto'>Order Information</h4>
@@ -52,24 +120,37 @@ const Parts = () => {
                                 <input type="text" name="phone" placeholder="Phone Number" className="input input-bordered w-full max-w-xs" />
 
                                 <input type="text" placeholder="Enter Your Address" class="input input-bordered input-lg w-full max-w-xs" />
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">Enter Quantity</span>
-                                    </label>
-                                    <label class="input-group">
-                                        <button className='btn btn-square' >
-                                        <i className="fa fa-plus fa-2x"></i>
-                                        </button>
-                                        <input type="text" placeholder="Type Here" class="input input-bordered" />
-                                        <button className='btn '>
-                                        <i className="fa fa-minus fa-2x"></i>
-                                        </button>                                    </label>
-                                </div>
+
                                 <input type="submit" value="Purchase" className="btn btn-secondary w-full max-w-xs" />
+                                <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Increase Quantity</span>
+                            </label>
+                            <label class="input-group">
+                                <input onChange={inputValue} type="text" placeholder="0.01" class="input input-bordered" />
+                                <p>{error}</p>
+                                <button  onClick={() => increaseStock(_id)} class="btn">Increase</button>
+                            </label>
+                        </div>
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Decrease Quantity</span>
+                            </label>
+                            <label class="input-group">
+                                <input type="text" placeholder="0.01" class="input input-bordered" />
+                                <button onClick={() => decreaseStock(_id)} class="btn">Decrease</button>
+                            </label>
+                        </div>
                             </form>
                         </div>
+
+                        
+
                     </div>
+                    
                 </div>
+                
+
             </div>
             <br />
             <br />
